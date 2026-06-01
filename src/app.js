042204887e33d0d298f78d6f33807c932b4fa477
Gaby-95 <gabrielaesculpi@gmail.com>
2026-06-01 02:18:39 +0000

    actualizando

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+let who = ['The dog', 'My mom', 'My dad', 'My boyfriend'];
+let action = ['ate', 'vomit', 'lost', 'broke'];
+let what = ['my homework', 'my phone', 'the computer'];
+let when = ['before the gym', 'when I was eating', 'while I was working', 'during my breakfast', 'while I was crying'];
+
+let whoIndex = Math.floor(Math.random() * who.length);
+let actionIndex = Math.floor(Math.random() * action.length);
+let whatIndex = Math.floor(Math.random() * what.length);
+let whenIndex = Math.floor(Math.random() * when.length);
+
+let randomExcuse = who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
+
+document.getElementById("excuse").innerHTML = randomExcuse;
+
+console.log("Excusa generada exitosamente: " + randomExcuse);
 };
